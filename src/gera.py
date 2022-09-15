627bf65ce8bcea2c7484d2adbe5ae20bcf4a634e
Tupi Guedes <tupiguedes@outlook.com>
2022-09-15 20:23:01 -0300

    Projeto iniciado

---
+++ src/gera.py
@@ -0,0 +1,30 @@
+import mediapipe as mp  # Trata o  reconhecimento da imagem
+import cv2  # Trata a imagem da camera
+
+camera = cv2.VideoCapture(0)
+
+# Um dos varios tipos de detectores da ferramenta
+forma_de_detectar = mp.solutions.face_detection
+reconhecedor = forma_de_detectar.FaceDetection()
+formato = mp.solutions.drawing_utils
+
+while True:
+    informacao, imagem = camera.read()
+
+    if not informacao:
+        break
+
+    rostos = reconhecedor.process(imagem)  # Lista de rostos que ele reconheceu
+
+    if rostos.detections:
+        for rosto in rostos.detections:
+            # Local e objeto que deseja reconhecer o rosto
+            formato.draw_detection(imagem, rosto)
+
+    cv2.imshow('Rostos na webcan', imagem)
+
+    if cv2.waitKey(5) == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
